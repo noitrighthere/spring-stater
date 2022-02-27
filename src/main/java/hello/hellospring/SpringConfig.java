ae945fdb3aeb28f3a90ac094389ecce6e348aa7b
nokh9503 <nokh9503@hanmail.net>
2022-02-27 20:57:25 +0900

    [추가] 스프링 DB 접근 기술

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,45 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 
 // configuration이 있으면 @Bean이 되어 있는 것을 스프링 빈 컨테이너에 등록한다.
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+    private final MemberRepository memberRepository;
+
+ /* JPA
+    private EntityManager em;
+ */
+
+ /* JDBC
+    private final DataSource dataSource;
+ */
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
+
+    /*
+    @Bean
+    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+    }
+     */
 }
